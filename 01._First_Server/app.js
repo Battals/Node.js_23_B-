6c9923d507d1cc458b75dc274d0654d1ed155db4
Battals <batt0006@stud.kea.dk>
2023-02-17 15:05:24 +0100

    Rest API Birds

--- 01._First_Server/app.js
+++ 01._First_Server/app.js
@@ -15,6 +15,31 @@ app.get("/kickdinosaur", (req, res) => {
     res.send({message: `You've kicked the dinosaur ${kick} times `})
 })
 
+const birds = [];
+birds.push([1, "one bird"]);
+birds.push([2, "two birds"]);
+birds.push([3, "three birds"]);
+birds.push([4, "four birds"]);
+birds.push([5, "five birds"]);
+
+
+
+// get all
+app.get("/", (req, res) => {
+    res.send(birds)
+})
+
+// get
+app.get("/:id", (req, res) => {
+    const id = req.params.id
+    birds.forEach(bird => {
+        if(bird[0] == id ){
+            console.log('bird found: ' + bird)
+            res.send(bird)
+        }
+    });
+})
+
 app.get("/about", (req, res) => {
     console.log(req.query)
     res.send(`
@@ -28,11 +53,11 @@ app.get("/bat", (req, res) =>  {
     
     res.send({message: `The bat is ${req.query.adjective} .`});
 });
-
+ 
 app.post("/package", (req, res) => {
     console.log()
     res.send({ message: req.body });
-})
+});
 
 app.get("/bottle/:bottleSize", (req,res) => {
     console.log(req.params);
